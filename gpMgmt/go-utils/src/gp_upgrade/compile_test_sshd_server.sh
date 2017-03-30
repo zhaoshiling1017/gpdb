5982d678aa82df66f36fee47d81a04bfa9fd5009
C.J. Jameson <cjameson@pivotal.io>
2017-03-30 09:59:39 -0700

    gp_upgrade: reduce noisy output in sshd server compile

--- gpMgmt/go-utils/src/gp_upgrade/compile_test_sshd_server.sh
+++ gpMgmt/go-utils/src/gp_upgrade/compile_test_sshd_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-pushd "./commands/sshd"
-go get
-go build -o "$GOPATH/bin/test/sshd"
-RET=$?
-popd
+pushd "./commands/sshd" 1>/dev/null
+  go get
+  go build -o "$GOPATH/bin/test/sshd"
+  RET=$?
+popd 1>/dev/null
 exit $RET

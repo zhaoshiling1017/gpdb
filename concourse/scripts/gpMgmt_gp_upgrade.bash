00e5a3d9ad720e3f9885e229a288420b6230d4c6
Nadeem Ghani <nghani@pivotal.io>
2017-10-11 14:56:47 -0700

    Fix test setup to avoid git malfunction
    Currenlty we source greenplum-path.sh before doing some git work, which
    fails because of the LD_LIBRARY_PATH setup in greenplum_path.sh.
    
    This commit runs the source commands after go get dependencies and
    changes how we call make_cluster to avoid this crosstalk.
    
    Signed-off-by: Marbin Tan <mtan@pivotal.io>

--- concourse/scripts/gpMgmt_gp_upgrade.bash
+++ concourse/scripts/gpMgmt_gp_upgrade.bash
@@ -8,13 +8,18 @@ source "${CWDIR}/common.bash"
 function gen_env(){
   cat > /opt/run_test.sh <<-EOF
 		base_path=\${1}
-		# source /usr/local/greenplum-db-devel/greenplum_path.sh
 		# source /opt/gcc_env.sh
-		# source \${base_path}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 		cd \${base_path}/gpdb_src/gpMgmt/go-utils/src/gp_upgrade
 		export GOPATH=\${base_path}/gpdb_src/gpMgmt/go-utils
 		export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin
 		make dependencies
+
+		# We need this to be after the make dependencies in order to
+		# be able to do a go get. Currently, if you source
+		# greenplum_path.sh and then run a git clone, you'll hit an
+		# issue 'exit 128'
+		source /usr/local/greenplum-db-devel/greenplum_path.sh
+		source \${base_path}/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 		make test
 	EOF
 
@@ -30,7 +35,7 @@ function _main() {
     configure
     install_gpdb
     setup_gpadmin_user
-    make_cluster
+    (make_cluster)
     gen_env
     run_test
 }

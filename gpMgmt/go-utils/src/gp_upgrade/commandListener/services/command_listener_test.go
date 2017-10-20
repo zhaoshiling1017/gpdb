a360ffebe46d5900fcd8dac89fb97aaeea49d26e
Nadeem Ghani <nghani@pivotal.io>
2017-10-20 14:35:02 -0700

    Remove integration test cruft from unit test
    Signed-off-by: C.J. Jameson <cjameson@pivotal.io>

--- gpMgmt/go-utils/src/gp_upgrade/commandListener/services/command_listener_test.go
+++ gpMgmt/go-utils/src/gp_upgrade/commandListener/services/command_listener_test.go
@@ -1,82 +1,33 @@
 package services_test
 
 import (
-	"io"
-	"net"
-
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
 	"gp_upgrade/commandListener/services"
-	"gp_upgrade/idl"
 	"gp_upgrade/utils"
 )
 
-var _ = Describe("CommandListenerManager", func() {
-	var (
-		manager    idl.CommandListenerServer
-		server     *grpc.Server
-		connCloser io.Closer
-		clsClient  idl.CommandListenerClient
-	)
-
-	var startGRPCServer = func(cls idl.CommandListenerServer) (*grpc.Server, string) {
-		lis, err := net.Listen("tcp", ":0")
-		Expect(err).ToNot(HaveOccurred())
-		s := grpc.NewServer()
-		idl.RegisterCommandListenerServer(s, cls)
-		go s.Serve(lis)
-
-		return s, lis.Addr().String()
-	}
-
-	var establishClient = func(clsAddr string) (idl.CommandListenerClient, io.Closer) {
-		conn, err := grpc.Dial(clsAddr, grpc.WithInsecure())
-		Expect(err).ToNot(HaveOccurred())
-		client := idl.NewCommandListenerClient(conn)
-
-		return client, conn
-	}
-
-	BeforeEach(func() {
-		var grpcAddr string
-		manager = services.NewCommandListener("foo")
-		server, grpcAddr = startGRPCServer(manager)
-		clsClient, connCloser = establishClient(grpcAddr)
-	})
-
-	AfterEach(func() {
-		server.Stop()
-		connCloser.Close()
-	})
-
-	It("connect me", func() {
-		request := idl.TransmitStateRequest{"transmit request"}
-		reply, err := clsClient.TransmitState(context.Background(), &request)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(reply.GetMessage()).To(Equal("Finished echo state request: transmit request foo"))
-	})
+var _ = Describe("CommandListener", func() {
 	Describe("check upgrade status", func() {
-		It("returns active", func() {
+		It("returns the shell command output", func() {
 			utils.System.ExecCmdOutput = func(name string, args ...string) ([]byte, error) {
-				return []byte("CHANGED"), nil
+				return []byte("shell command output"), nil
 			}
 			listener := services.NewCommandListener("some string")
-			_, err := listener.CheckUpgradeStatus(nil, nil)
+			resp, err := listener.CheckUpgradeStatus(nil, nil)
+			Expect(resp.ProcessList).To(Equal("shell command output"))
 			Expect(err).To(BeNil())
 		})
-	})
-	Describe("check upgrade status", func() {
-		It("returns err", func() {
+
+		It("returns only err if anything is reported as an error", func() {
 			utils.System.ExecCmdOutput = func(name string, args ...string) ([]byte, error) {
-				return []byte("CHANGED"), errors.New("couldn't find bash")
+				return []byte("stdout during error"), errors.New("couldn't find bash")
 			}
 			listener := services.NewCommandListener("some string")
-			resp, _ := listener.CheckUpgradeStatus(nil, nil)
+			resp, err := listener.CheckUpgradeStatus(nil, nil)
 			Expect(resp).To(BeNil())
+			Expect(err.Error()).To(Equal("couldn't find bash"))
 		})
 	})
 })
